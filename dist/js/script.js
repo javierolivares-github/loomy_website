e3715f7ec7f0f6a4e0e47ddb1a271c00312ff906
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-07-02 10:58:28 -0400

    avances 02072021

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,4 @@
-// Función obtener todos los articulos
+// Obtener todos los articulos.
 function obtenerArticulos () {
   fetch('./db/db.json')
   .then(res => res.json())
@@ -33,20 +33,21 @@ function obtenerArticulos () {
       `;
     });
 
+    // Salida pantalla
     document.getElementById('blog').innerHTML = salidaArticulos;
   })
+  .catch (err => console.log('Error: ' + err.message));
 };
 
-// Función para obtener articulos filtrados por categoria de Tejidos.
-function obtenerCategoriaTejidos(){
+// Obtener articulos filtrados por categoria de Tejidos.
+function obtenerArticulosPorCategoriaTejidos(){
   fetch('./db/db.json')
   .then(res => res.json())
   .then(data => {
-    console.log(data);
     
     // Filtrando articulos con categoria Tejidos.
     let categoriaTejidos = data.filter(articulo => articulo.category === 'Tejidos');
-    console.log(categoriaTejidos);
+
     
     // Creando una salida para los articulos fltrados por Tejidos.
     let salidaCategoriaTejidos = '';
@@ -54,46 +55,119 @@ function obtenerCategoriaTejidos(){
     categoriaTejidos.forEach(function(articulo){
       salidaCategoriaTejidos += `
       <article class="article">
-        
-      <h2 class="article__title">${articulo.title}</h2>
-    
-      <div class="article__imgContainer">
-        <a href="article/article01.html">
-          <img src="${articulo.img}" alt="tintes" class="article__img">
-        </a>
-      </div>
-      
-      <div class="article__badgeContainer">
-      <span class="badge bg-warning text-dark article__badge">${articulo.author}</span>
-      <span class="badge bg-danger article__badge">${articulo.date}</span>
-      <span class="badge bg-info article__badge">${articulo.read_time}</span>
-      <span class="badge bg-primary article__badge">${articulo.category}</span>
-      </div>
-      
-      <p class="article__paragraph">${articulo.content}</p>
-      
-      <div>
-        <a href="article/article01.html" class="btn--primary">Leer más</a>
-      </div>
-    </article>
+        <h2 class="article__title">${articulo.title}</h2>
+        <div class="article__imgContainer">
+          <a href="article/article01.html">
+            <img src="${articulo.img}" alt="tintes" class="article__img">
+          </a>
+        </div>
+        <div class="article__badgeContainer">
+        <span class="badge bg-warning text-dark article__badge">${articulo.author}</span>
+        <span class="badge bg-danger article__badge">${articulo.date}</span>
+        <span class="badge bg-info article__badge">${articulo.read_time}</span>
+        <span class="badge bg-primary article__badge">${articulo.category}</span>
+        </div>
+        <p class="article__paragraph">${articulo.content}</p>
+        <div>
+          <a href="article/article01.html" class="btn--primary">Leer más</a>
+        </div>
+      </article>
       `;
     });
 
+    // Salida pantalla
     document.getElementById('blog').innerHTML = salidaCategoriaTejidos;
-  });
+  })
+  .catch (err => console.log('Error: ' + err.message));
 };
 
-// Función para obtener articulos filtrados por categoria de Crianza.
-function obtenerCategoriaCrianza (){
+// Obtener articulos filtrados por categoria de Crianza.
+function obtenerArticulosPorCategoriaCrianza (){
   fetch('./db/db.json')
   .then(res => res.json())
   .then(data => {
-    console.log(data);
+      // Filtrando por Crianza
+      const categoriaCrianza = data.filter(articulo => articulo.category === 'Crianza');
+      
+      // Creando una salida para estos articulos.
+      let salidaCategoriaCrianza = '';
+
+      categoriaCrianza.forEach(function(articulo){
+        salidaCategoriaCrianza += `
+          <article class="article">
+            <h2 class="article__title">${articulo.title}</h2>
+            <div class="article__imgContainer">
+              <a href="article/article01.html">
+                <img src="${articulo.img}" alt="tintes" class="article__img">
+              </a>
+            </div>
+            <div class="article__badgeContainer">
+            <span class="badge bg-warning text-dark article__badge">${articulo.author}</span>
+            <span class="badge bg-danger article__badge">${articulo.date}</span>
+            <span class="badge bg-info article__badge">${articulo.read_time}</span>
+            <span class="badge bg-primary article__badge">${articulo.category}</span>
+            </div>
+            <p class="article__paragraph">${articulo.content}</p>
+            <div>
+              <a href="article/article01.html" class="btn--primary">Leer más</a>
+            </div>
+          </article>
+        `;
+      });
+
+      // Salida pantalla
+      document.getElementById('blog').innerHTML = salidaCategoriaCrianza;
   })
+  .catch (err => console.log('Error: ' + err.message));
 };
 
+// Obtener articulos filtrados por autor Silvana.
+function obtenerArticulosPorAutorSilvana() {
+  fetch('./db/db.json')
+  .then(res => res.json())
+  .then(data => {
+    
+    // Filtrando por autor Silvana Vitali
+    const articulosDeSilvana = data.filter(articulo => articulo.author === 'Silvana Vitali');
+    console.log(articulosDeSilvana);
+
+    // Creando una plantilla para salida
+    let salidaArticulosDeSilvana = '';
+
+    articulosDeSilvana.forEach(articulo => {
+      salidaArticulosDeSilvana += `
+      <article class="article">
+        <h2 class="article__title">${articulo.title}</h2>
+        <div class="article__imgContainer">
+          <a href="article/article01.html">
+            <img src="${articulo.img}" alt="tintes" class="article__img">
+          </a>
+        </div>
+        <div class="article__badgeContainer">
+          <span class="badge bg-warning text-dark article__badge">${articulo.author}</span>
+          <span class="badge bg-danger article__badge">${articulo.date}</span>
+          <span class="badge bg-info article__badge">${articulo.read_time}</span>
+          <span class="badge bg-primary article__badge">${articulo.category}</span>
+        </div>
+        <p class="article__paragraph">${articulo.content}</p>
+        <div>
+          <a href="article/article01.html" class="btn--primary">Leer más</a>
+        </div>
+      </article>
+      `;
+    });
+
+    // Mostrando la salida en pantalla
+    document.getElementById('blog').innerHTML = salidaArticulosDeSilvana;
+
+  })
+  .catch (err => console.log('Error: ' + err.message));
+};
+
+
 // obtenerArticulos();
-obtenerCategoriaTejidos(); 
-obtenerCategoriaCrianza(); 
+// obtenerArticulosPorCategoriaTejidos(); 
+// obtenerArticulosPorCategoriaCrianza();
+// obtenerArticulosPorAutorSilvana(); 
 
 
